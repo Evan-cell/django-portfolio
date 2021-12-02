0c81e7656f4e9f2902868e4f7dd3e8563fbf3c1d
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2021-12-02 16:24:05 +0300

    register

--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
    return render(request, 'base/home.html')
+def registerPage(request):
+    context = {}
+    return render(request, 'accounts/register.html', context)
